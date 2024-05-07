c3071c1b9f6704aa1e247e6fe6c1988f3c89a9ac
250304s <250304s@ugs.kochi-tech.ac.jp>
2024-05-07 12:23:15 +0900

    授業資料完成

---
+++ lesson4/lesson4_1.py
@@ -0,0 +1,14 @@
+list1 = [1, 2, 3, 4, 5]
+print(list1)
+print(list1[0])
+print(list1[4])
+list1[0] = 10
+print(list1)
+list1.append(6)
+print(list1)
+del list1[0]
+print(list1)
+list1.insert(3, 0)
+print(list1)
+list1.remove(3)
+print(list1)
---
+++ lesson4/lesson4_2.py
@@ -0,0 +1,5 @@
+dict1 = {"name": "Alice", "age": 14, "birthday": "2010-04-01"}
+print(dict1)
+print(dict1["name"])
+print(dict1["age"])
+print(dict1["birthday"])
